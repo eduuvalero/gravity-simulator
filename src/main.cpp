19229115c53115ceed001ed46440ced8b798db52
Eduardo Valero <eduvalero180@gmail.com>
2026-08-06 14:27:44 +0200

    OpenGL test main (This should be ina  dev branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,230 +1,42 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include "engine/Physics.h"
-#include "engine/Body.h"
 
+#include "config/Config.h"
 
-void printBody(const std::string& name, const Body& b)
-{
-    std::cout
-        << name
-        << " | id: " << b.id
-        << " | alive: " << b.alive
-        << " | mass: " << b.mass
-        << " | radius: " << b.radius
-        << " | pos: " << b.position
-        << " | vel: " << b.velocity
-        << "\n";
-}
+using namespace std;
 
-
-Body createBody(
-    BodyType type,
-    double mass,
-    double radius,
-    Vector3d position,
-    Vector3d velocity
-)
-{
-    Body b;
-
-    b.type = type;
-    b.mass = mass;
-    b.radius = radius;
-
-    b.position = position;
-    b.velocity = velocity;
-    b.acceleration = {0,0,0};
-
-    return b;
-}
-
-
-void simulate(
-    Physics& physics,
-    int steps,
-    double dt
-)
-{
-    for(int i = 0; i < steps; i++)
-    {
-        physics.step(dt);
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+        glViewport(0, 0, width, height);
     }
-}
 
+int main(){
 
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-int main()
-{
-    constexpr double dt = 0.01;
-
-
-
-    // ==============================
-    // PLANET + PLANET
-    // ==============================
-
-    std::cout << "\n=== PLANET COLLISION ===\n";
-
-    {
-        Physics physics;
-
-
-        Body planet1 = createBody(
-            BodyType::Planet,
-            10,
-            2,
-            {-5,0,0},
-            {5,0,0}
-        );
-
-
-        Body planet2 = createBody(
-            BodyType::Planet,
-            10,
-            2,
-            {5,0,0},
-            {-5,0,0}
-        );
-
-
-        physics.addBody(&planet1);
-        physics.addBody(&planet2);
-
-
-        simulate(physics, 200, dt);
-
-
-        printBody("Planet 1", planet1);
-        printBody("Planet 2", planet2);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Gravity Simulator", nullptr, nullptr);
+    if(window == nullptr){
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
     }
 
 
+    glfwMakeContextCurrent(window);
 
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // ==============================
-    // ASTEROID + ASTEROID
-    // ==============================
-
-    std::cout << "\n=== ASTEROID BOUNCE ===\n";
-
-    {
-        Physics physics;
-
-
-        Body asteroid1 = createBody(
-            BodyType::Asteroid,
-            1,
-            1,
-            {-3,0,0},
-            {5,0,0}
-        );
-
-
-        Body asteroid2 = createBody(
-            BodyType::Asteroid,
-            1,
-            1,
-            {3,0,0},
-            {-5,0,0}
-        );
-
-
-        physics.addBody(&asteroid1);
-        physics.addBody(&asteroid2);
-
-
-        simulate(physics, 150, dt);
-
-
-        printBody("Asteroid 1", asteroid1);
-        printBody("Asteroid 2", asteroid2);
+    while (!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
-
-
-
-    // ==============================
-    // STAR + PLANET
-    // ==============================
-
-    std::cout << "\n=== STAR ABSORB PLANET ===\n";
-
-    {
-        Physics physics;
-
-
-        Body star = createBody(
-            BodyType::Star,
-            1000,
-            5,
-            {0,0,0},
-            {0,0,0}
-        );
-
-
-        Body planet = createBody(
-            BodyType::Planet,
-            10,
-            2,
-            {8,0,0},
-            {-10,0,0}
-        );
-
-
-        physics.addBody(&star);
-        physics.addBody(&planet);
-
-
-        simulate(physics, 100, dt);
-
-
-        printBody("Star", star);
-        printBody("Planet", planet);
-    }
-
-
-
-
-    // ==============================
-    // STAR + STAR
-    // ==============================
-
-    std::cout << "\n=== STAR MERGE ===\n";
-
-    {
-        Physics physics;
-
-
-        Body star1 = createBody(
-            BodyType::Star,
-            500,
-            3,
-            {-5,0,0},
-            {5,0,0}
-        );
-
-
-        Body star2 = createBody(
-            BodyType::Star,
-            1500,
-            3,
-            {5,0,0},
-            {-5,0,0}
-        );
-
-
-        physics.addBody(&star1);
-        physics.addBody(&star2);
-
-
-        simulate(physics, 150, dt);
-
-
-        printBody("Star 1", star1);
-        printBody("Star 2", star2);
-    }
-
+    glfwTerminate();
 
     return 0;
 }
